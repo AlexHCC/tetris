02221162e6680a10927188fb92343e0459ca50b5
AlexHCC <alexander06088@yahoo.com>
2017-06-27 23:52:19 +0300

    Project base

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\033[1;31m" << " Welcome to tetris!" << "\033[0m" << endl;
+     cout << "" << endl;
+    cout << " ■ ■ ■" << endl;
+    cout << " ■" << endl;
+    return 0;
+}
