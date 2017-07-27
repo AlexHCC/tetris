40414acf6ab335c0e55c9b2326268de148c5533a
neobis001 <nijobias@gmail.com>
2017-07-27 16:59:43 -0700

    removing debug stuff
    will only put in personal folder and leave main files as committed ones

--- debug_tools.cpp
+++
@@ -1,9 +0,0 @@
-#include "debug_tools.h"
-
-void debug_msg(const char * details) {
-    cout << "DEBUG MESSAGE" << endl;
-    cout << "DETAILS: " << details << endl;
-    cout << "Enter a random integer to continue" << endl;
-    int random;
-    cin >> random;
-}
